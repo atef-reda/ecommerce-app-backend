b22366b4c331e73e35897cbcddb4520e586310a3
atef reda <redaatef329@gmail.com>
2025-02-25 08:01:59 +0200

    1st commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php 
+
+include 'connect.php';
+sendEmail("redaatef329@gmail.com","hi","from php function");
+// $table = "users";
+// $name = filterRequest("namerequest");
+// $data = array( 
+// "users_name" => "wael",
+// "users_email" => "wael@gmail.com",
+// "users_phone" => "324234",
+// "users_verfiycode" => "3243243",
+
+// );
+// $count = insertData($table , $data);
