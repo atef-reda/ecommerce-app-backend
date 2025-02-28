1c6c65ad231d7e8118330eb4afd564155d4c272b
atef reda <redaatef329@gmail.com>
2025-02-28 10:42:45 +0200

    verify code

---
+++ auth/verifycode.php
@@ -0,0 +1,17 @@
+<?php
+
+include "../connect.php";
+
+$email=filterRequest("email");
+$verifycode=filterRequest("verifycode");
+
+$stmt=$con->prepare("SELECT * FROM users WHERE users_email = '$email' AND users_verfiycode = '$verifycode'");
+$stmt->execute();
+$count=$stmt->rowCount();
+
+if($count>0){
+    $data=array("users_approve" => "1");
+    updateData("users",$data,"users_email = '$email' ");
+}else{
+    printFailure("verify code not correct");
+}
